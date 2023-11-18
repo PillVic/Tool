760ea16fea75368eb8722e7d7a32c3cf6ac7b5d7
neovic <16205111@hdu.edu.cn>
2023-11-18 23:40:24 +0800

    random-select:随机选择一个文件行

---
+++ random-select.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+#随机选择一个文件行
+FILE_NAME=$1
+
+if [ -z "$FILE_NAME" ]; then
+    echo "empty file name!"
+    exit 1
+fi
+
+TOTAL_LINE="$(wc -l "${FILE_NAME}"|awk '{print $1}')"
+
+RANDOM_NUMBER=$RANDOM
+
+SELECTED_LINE_NUMBER="$(((RANDOM_NUMBER%TOTAL_LINE)+1))"
+
+SED_ARGS="-n ${SELECTED_LINE_NUMBER}p"
+
+SELECTED_LINE="$(sed -n $SED_ARGS $FILE_NAME)"
+
+
+#echo "$FILE_NAME $TOTAL_LINE lines, random: $RANDOM_NUMBER, SELECTED_LINE_NUMBER: $SELECTED_LINE_NUMBER"
+
+#echo "SELECTED_LINE: ${SELECTED_LINE}"
+echo $SELECTED_LINE
