5469dfc8d97d65d5ac2c691ccfdf2e9bf383e184
neovic <16205111@hdu.edu.cn>
2024-07-21 08:35:56 +0800

    日志缓存500M

--- daily.sh
+++ daily.sh
@@ -1,12 +1,15 @@
 #!/bin/env bash
 
-#desc 每日更新软件脚本
+#desc 系统例行任务脚本
 
 #x: print the command before excute it
 #e: once a command fail (return no zero)
 #u: when using undefined variable, exit
 #o pipefail: once a single pipe fail exit
 set -xeuo pipefail
+
+#限制日志缓存
+sudo journalctl --vacuum-size=500M
 
 yay -Syu
 
